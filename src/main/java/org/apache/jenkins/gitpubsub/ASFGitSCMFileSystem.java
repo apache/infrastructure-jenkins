b77a6096258460fe508fa86af0265146eb3a6e75
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-11-13 10:40:41 +0000

    Episode 8

--- src/main/java/org/apache/jenkins/gitpubsub/ASFGitSCMFileSystem.java
+++ src/main/java/org/apache/jenkins/gitpubsub/ASFGitSCMFileSystem.java
@@ -8,19 +8,30 @@ import java.io.IOException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jenkins.plugins.git.AbstractGitSCMSource;
 import jenkins.plugins.git.GitSCMTelescope;
+import jenkins.plugins.git.GitTagSCMHead;
+import jenkins.plugins.git.GitTagSCMRevision;
 import jenkins.scm.api.SCMFile;
 import jenkins.scm.api.SCMFileSystem;
 import jenkins.scm.api.SCMHead;
 import jenkins.scm.api.SCMRevision;
+import org.eclipse.jgit.lib.Constants;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ASFGitSCMFileSystem extends SCMFileSystem {
+
+    private static final Logger LOGGER = Logger.getLogger(ASFGitSCMFileSystem.class.getName());
 
     /**
      * Constructor.
@@ -108,19 +119,108 @@ public class ASFGitSCMFileSystem extends SCMFileSystem {
         public SCMRevision getRevision(@NonNull String remote, StandardCredentials credentials,
                                        @NonNull String refOrHash)
                 throws IOException, InterruptedException {
-            return null;
+            String commitUrl = buildTemplateWithRemote("{+server}{?p}{;a,h}", remote)
+                    .set("a", "commit")
+                    .set("h", refOrHash)
+                    .expand();
+            Document doc = Jsoup.parse(new URL(commitUrl), TEN_SECONDS_OF_MILLIS);
+            Elements elements = doc.select("table.object_header tr td.sha1");
+            String revision = elements.get(0).text();
+            if (refOrHash.startsWith(Constants.R_TAGS)) {
+                elements = doc.select("table.object_header tr td span.datetime");
+                long time;
+                try {
+                    time =
+                            new SimpleDateFormat(ASFGitSCMNavigator.RFC_2822).parse(elements.get(1).text()).getTime();
+                } catch (ParseException e) {
+                    throw new IOException("Unexpected date format, expected RFC 2822, got " + elements.get(1).text());
+                }
+                return new GitTagSCMRevision(new GitTagSCMHead(refOrHash.substring(Constants.R_TAGS.length()), time), revision);
+            } else if (refOrHash.startsWith(Constants.R_HEADS)){
+                return new AbstractGitSCMSource.SCMRevisionImpl(new SCMHead(refOrHash.substring(Constants.R_HEADS.length())), revision);
+            } else {
+                // TODO fix for hash without branch name
+                return null;
+            }
         }
 
         @Override
         public Iterable<SCMRevision> getRevisions(@NonNull String remote, StandardCredentials credentials,
                                                   @NonNull Set<ReferenceType> referenceTypes)
                 throws IOException, InterruptedException {
-            return null;
+            List<String> result = new ArrayList<>();
+            TYPES:
+            for (ReferenceType referenceType : referenceTypes) {
+                String actionUrl;
+                Document doc;
+                Elements elements;
+                String prefix;
+                switch (referenceType) {
+                    case HEAD:
+                        actionUrl = buildTemplateWithRemote("{+server}{?p}{;a}", remote)
+                                .set("a", "heads")
+                                .expand();
+                        doc = Jsoup.parse(new URL(actionUrl), TEN_SECONDS_OF_MILLIS);
+                        elements = doc.select("table.heads tr td a.name");
+                        prefix = Constants.R_HEADS;
+                        break;
+                    case TAG:
+                        actionUrl = buildTemplateWithRemote("{+server}{?p}{;a}", remote)
+                                .set("a", "tags")
+                                .expand();
+                        doc = Jsoup.parse(new URL(actionUrl), TEN_SECONDS_OF_MILLIS);
+                        elements = doc.select("table.tags tr td a.name");
+                        prefix = Constants.R_TAGS;
+                        break;
+                    default:
+                        LOGGER.log(Level.WARNING, "Ignoring unexpected reference type {0}", referenceType);
+                        continue TYPES;
+                }
+                for (Element element : elements) {
+                    result.add(prefix + element.text());
+                }
+            }
+            return new AbstractList<SCMRevision>() {
+                private final List<SCMRevision> cache = new ArrayList<>(result.size());
+
+                {
+                    for (int i = 0; i < result.size(); i++) {
+                        cache.add(null);
+                    }
+                }
+
+                @Override
+                public SCMRevision get(int index) {
+                    SCMRevision r = cache.get(index);
+                    if (r == null) {
+                        try {
+                            r = getRevision(remote, credentials, result.get(index));
+                        } catch (IOException | InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        cache.set(index, r);
+                    }
+                    return r;
+                }
+
+                @Override
+                public int size() {
+                    return result.size();
+                }
+            };
         }
 
         @Override
         public String getDefaultTarget(@NonNull String remote, StandardCredentials credentials)
                 throws IOException, InterruptedException {
+            String commitUrl = buildTemplateWithRemote("{+server}{?p}{;a}", remote)
+                    .set("a", "heads")
+                    .expand();
+            Document doc = Jsoup.parse(new URL(commitUrl), TEN_SECONDS_OF_MILLIS);
+            Elements elements = doc.select("table.heads tr td.current_head a.name");
+            if (elements.size()>0) {
+                return Constants.R_HEADS+elements.get(0).text();
+            }
             return null;
         }
     }
--- src/test/java/org/apache/jenkins/gitpubsub/ASFGitSCMFileSystemTest.java
+++ src/test/java/org/apache/jenkins/gitpubsub/ASFGitSCMFileSystemTest.java
@@ -1,5 +1,8 @@
 package org.apache.jenkins.gitpubsub;
 
+import java.util.EnumSet;
+import jenkins.plugins.git.GitSCMTelescope;
+import jenkins.scm.api.SCMRevision;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -14,5 +17,14 @@ public class ASFGitSCMFileSystemTest {
         long timestamp = new ASFGitSCMFileSystem.TelescopeImpl()
                 .getTimestamp("https://git-wip-us.apache.org/repos/asf/maven.git", null, "maven-3.5.0");
         assertThat(timestamp, is(1491248130000L));
+    }
+    @Test
+    public void smokeTestGetRevisions() throws Exception {
+        Iterable<SCMRevision> revisions = new ASFGitSCMFileSystem.TelescopeImpl()
+                .getRevisions("https://git-wip-us.apache.org/repos/asf/maven.git", null, EnumSet.allOf(
+                        GitSCMTelescope.ReferenceType.class));
+        for (SCMRevision r: revisions) {
+            System.out.println(r);
+        }
     }
 }
